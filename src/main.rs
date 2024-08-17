09be20ce85180946cf83794819321c5579a8e783
Gabriel Ford <gabe@fordltc.net>
2024-08-17 17:30:45 -0400

    Shorten the result checking by returning error

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,21 @@
 use std::env;
-use std::fs;
 use std::error::Error;
+use std::fs;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
-    let query = args.get(1);
-    if let None = query {
-        println!("Nothing was specified for the query param!!!");
-        panic!();
-    }
-    let query = query.unwrap();
+    let query = args
+        .get(1)
+        .expect("Nothing was specified for the query param!!!");
 
-    let filepath = args.get(2);
-    if let None = filepath {
-        println!("Nothing was specified for the filepath param!!!");
-        panic!();
-    }
-    let filepath = filepath.unwrap();
-
-    println!(
-        "Thank you for frepping with me, the query is '{query}', \
-        and the filepath is '{filepath}'"
-    );
+    let filepath = args
+        .get(2)
+        .expect("Nothing was specified for the filepath param!!!");
 
     let contents = fs::read_to_string(filepath)?;
 
-    println!("With text:\n{contents}");
+    eprintln!("With text:\n{contents}");
 
     Ok(())
 }
