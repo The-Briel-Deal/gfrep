adf961a23c759aa8e594a509f910be9b1904e3f3
Gabriel Ford <gabe@fordltc.net>
2024-08-17 21:48:23 -0400

    Tested and finished!

---
+++ src/lib.rs
@@ -0,0 +1,95 @@
+use std::fs;
+
+pub struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        let query = args
+            .get(1)
+            .ok_or("Nothing was specified for the query param!!!")?
+            .clone();
+
+        let file_path = args
+            .get(2)
+            .ok_or("Nothing was specified for the filepath param!!!")?
+            .clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), std::io::Error> {
+    let contents = fs::read_to_string(config.file_path)?;
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+    Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+                        Rust:\n\
+                        safe, fast, productive.\n\
+                        Pick three.\n\
+                       ";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn three_results() {
+        let query = "o";
+        let contents = "\
+                        Dogs:\n\
+                        big, girthy, monsters.\n\
+                        Oh, and don't forget greasy.\n\
+                        Pick 4.\n\
+                       ";
+        assert_eq!(
+            vec![
+                "Dogs:",
+                "big, girthy, monsters.",
+                "Oh, and don't forget greasy."
+            ],
+            search(query, contents)
+        );
+    }
+    #[test]
+    fn numerical_one_result() {
+        let query = "4";
+        let contents = "\
+                        Dogs:\n\
+                        big, girthy, monsters.\n\
+                        Oh, and don't forget greasy.\n\
+                        Pick 4.\n\
+                       ";
+        assert_eq!(vec!["Pick 4."], search(query, contents));
+    }
+
+    #[test]
+    fn wrong_case_no_result() {
+        let query = "pick";
+        let contents = "\
+                        Dogs:\n\
+                        big, girthy, monsters.\n\
+                        Oh, and don't forget greasy.\n\
+                        Pick 4.\n\
+                       ";
+        assert_eq!(vec![] as Vec<&str>, search(query, contents));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,13 @@
+use gfrep;
 use std::env;
 use std::error::Error;
-use std::fs;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args)?;
+    let config = gfrep::Config::build(&args)?;
 
-    let contents = fs::read_to_string(config.file_path)?;
-
-    eprintln!("With text:\n{contents}");
+    gfrep::run(config)?;
 
     Ok(())
 }
 
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        let query = args
-            .get(1)
-            .ok_or("Nothing was specified for the query param!!!")?
-            .clone();
-
-        let file_path = args
-            .get(2)
-            .ok_or("Nothing was specified for the filepath param!!!")?
-            .clone();
-
-        Ok(Config { query, file_path })
-    }
-}
-
-// Returns a tuple of (query, filepath) if they both exist.
-fn parse_config(args: &[String]) -> Result<Config, &str> {
-    let query = args
-        .get(1)
-        .ok_or("Nothing was specified for the query param!!!")?
-        .clone();
-
-    let file_path = args
-        .get(2)
-        .ok_or("Nothing was specified for the filepath param!!!")?
-        .clone();
-
-    Ok(Config { query, file_path })
-}
